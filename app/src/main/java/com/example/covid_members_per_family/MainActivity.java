197a0992707f6599518de08b979ce694c5f8c933
Rafael Dalvas <rafaeldalvas@gmail.com>
2020-11-29 22:04:50 -0300

    main activity btnCadastrar

--- app/src/main/java/com/example/covid_members_per_family/MainActivity.java
+++ app/src/main/java/com/example/covid_members_per_family/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.covid_members_per_family;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCadastrar = (Button) findViewById(R.id.btn_cadastrar);
+        btnCadastrar.setOnClickListener(new android.view.View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
